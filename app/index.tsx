012fc85d7f37659d794e4637a8ce97dd1731d37c
IKartiKI <ks.kartik1610@gmail.com>
2026-01-07 12:23:03 +0530

    Initialised a basic react-native project

---
+++ app/index.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+//import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
+
+interface Pokemon{
+  name: string;
+  image: string;
+  imageBack: string;
+}
+
+export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  useEffect(() => {
+    //fetch pokemons
+    fetchPokemons()
+  }, [])
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20"
+      );
+      const data = await response.json();
+
+      //Fetch detailed info for each Pokemon in parallel
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default,
+            imageBack: details.sprites.back_default,
+          }
+        })
+      )
+
+      setPokemons(detailedPokemons);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key = {pokemon.name}>
+          <Text>{pokemon.name}</Text>
+
+          <View
+            style = {{ 
+              flexDirection: 'row', justifyContent: 'space-between', width: 400
+            }}
+          >
+            <Image
+              source = {{ uri: pokemon.image}}
+              style = {{ width:190, height: 190}}
+            />
+            <Image
+              source = {{ uri: pokemon.imageBack}}
+              style = {{ width:190, height: 190}}
+            />
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
